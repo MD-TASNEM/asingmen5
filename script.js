b054b8e8723b08b7e77512ce5a4a5e0371b6819f
MD.TASNEM <tasnimmuhammad51@gmail.com>
2025-09-08 05:52:09 +0600

    include Categories

--- script.js
+++ script.js
@@ -42,70 +42,23 @@ loadcards();
 
 
 
-// const cardsContainer = document.getElementById("cards");
+const categoryContainer = document.getElementById("categoryContainer");
 
-// const loadcards = () => {
-//   fetch(`https://openapi.programming-hero.com/api/categories`)
-//     .then((res) => res.json())
-//     .then((data) => {
-//       console.log(data.categories);
-//       const cards = data.plants;
-//       cards.forEach(card => {
-//         cardsContainer.innerHTML += `
-//                 <div>
-//                   <img
-//                   class="w-full h-48 object-cover rounded-t-lg"
-//                   src="./assets/about.png"
-//                   alt="Mango Tree"
-//                 />
-//                 <div class="py-4 px-2 w-full flex flex-col gap-2">
-//                   <p class="font-bold py-2">Mango Tree</p>
-//                   <p class="text-sm text-justify">
-//                     A fast-growing tropical tree that produces delicious, juicy
-//                     mangoes during summer.
-//                   </p>
-//                   <div class="flex justify-between items-center my-2">
-//                     <span
-//                       class="font-bold text-sm py-1 px-3 bg-green-200 rounded-full"
-//                       >Mango Tree</span
-//                     >
-//                     <span class="font-bold text-xl">৳500</span>
-//                   </div>
-//                   <button
-//                     class="text-sm bg-green-700 px-4 py-2 rounded-full text-green-50 font-bold w-full hover:bg-yellow-700 transition-colors duration-300"
-//                   >
-//                     Add to Cart
-//                   </button>
-//                 </div>
-//                 </div>
-//             `;
-//       });
-//     })
-//     .catch((err) => {
-//       console.log(err);
-//     });
-// };
+const loadcategory = () => {
+  fetch(`https://openapi.programming-hero.com/api/categories`)
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data.categories);
+      const categories = data.categories;
+      categories.forEach(category => {
+        categoryContainer.innerHTML += `
+            <li class="p-1 hover:bg-green-700 rounded-sm">${category.category_name}</li>
+            `;
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
 
-// loadcategory();
-
-
-// const categoryContainer = document.getElementById("categoryContainer");
-
-// const loadcategory = () => {
-//   fetch(`https://openapi.programming-hero.com/api/categories`)
-//     .then((res) => res.json())
-//     .then((data) => {
-//       console.log(data.categories);
-//       const categories = data.categories;
-//       categories.forEach(category => {
-//         categoryContainer.innerHTML += `
-//             <li class="p-1 hover:bg-green-700 rounded-sm">${category.category_name}</li>
-//             `;
-//       });
-//     })
-//     .catch((err) => {
-//       console.log(err);
-//     });
-// };
-
-// loadcategory();
+loadcategory();
